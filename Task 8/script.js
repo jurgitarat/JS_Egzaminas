af3037279523740f7bf256cde81abe5dc67b6130
Jurgita Ratkeliene <jurgita.ratkeliene@codeacademylt.onmicrosoft.com>
2022-09-09 22:08:49 +0300

    Task 6 complete

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,4 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
